5ab8bfea5f5753efd33818edc976bb80bfd31308
PippoGit <filipscotto@gmail.com>
2019-03-21 18:59:45 +0100

    things

--- public/js/script.js
+++ public/js/script.js
@@ -1,112 +1,38 @@
 var CMD_CONFIG;
-var FAVS;
 var news;
-var redditing = false, currentsub = "";
-var currentArticle = -1;
 
 function more() {
-	if(redditing) loadSubReddit(currentsub, 100);
-    else
-    loadNews(100);
+  loadNews(100);
 }
 
 function readArticle(index) {
-   currentArticle = index;
-	
-   	var swalbuttons = {
-      read: "Articolo completo",
-      cancel: "Chiudi",
-    };
-    
-	if(redditing) {
-		swalbuttons = {
-         read: "Articolo completo",
-         rcomments : "Commenti",
-         cancel: "Chiudi"
-       };    
-    }
-
-  swal({
-    title: news[index].title,
-    text: news[index].description,
-    icon: (news[index].urlToImage).replace("&amp;", "&"),
-
-    animation: false,
-     buttons: swalbuttons,
-  }).then((result) => {
-    switch(result) {
-    	case "read":
-     		var win = window.open(news[index].url, '_blank');
-            break;
-        case "rcomments":
-        	var win = window.open("https://reddit.com" + news[index].permalink, '_blank');
-            break;
-        default:
-      		currentArticle = -1;
-    }
-  })
+  var win = window.open(news[index].url, '_blank');
 }
 
 function loadArticlesFromUrl(url) {
   $.get(url, function(data) {
     var list = $("#feed");
     list.empty();
-    news = (redditing)?data.data.children:data.articles;
+    news = data.articles;
 
     for(var i=0; i<news.length;i++)
     {
-      var imgUrl;
-      if(redditing) {
-      	news[i] = news[i].data;
-        news[i].source = {};
-        news[i].source.name = (news[i].domain.startsWith("self."))? "r": news[i].domain;
-      	news[i].urlToImage = (news[i].preview == undefined)?null:news[i].preview.images[0].source.url;
-      }
-      
+      var imgUrl;      
       imgUrl = (news[i].urlToImage == null)?"-webkit-linear-gradient(top, #fd0b58 0px, #a32b68 100%)":news[i].urlToImage;
       list.append("<li onclick='readArticle("+ i + ")' class='deck' data-index='" + i + "' > <div class='card card-news'><div class='list-header'> <img class='list-img' src='"+ imgUrl + "'></img> <div class='list-category'>"+news[i].source.name +"</div> <div class='list-title'>" + news[i].title + "</div><div class='list-author'>"+ news[i].author +"</div> <div class='list-datetime'> <i class='fas fa-clock'></i> " + news[i].publishedAt + "</div><div class='list-content'>"+ news[i].description + "</div> <div class='list-footer'> <i class='far fa-thumbs-up likebtn'></i> | <i class='far fa-thumbs-down dislikebtn'></i></div></div></li>");
     }
   });
 }
 
-function redditArticle (i) {
-	var win = window.open(news[i].url, '_blank');
-}
-
-function loadSubReddit(sub, pageSize = 30) {
-	var url = 'https://www.reddit.com/r/' + (sub) + "/.json?count=" + pageSize + "&limit=" + pageSize;
-  	redditing = true;
-    currentsub = sub;
-    loadArticlesFromUrl(url);
-}
-
 function loadNews(pageSize = 30) {
   //NEWS API: 3091d29cdba743aa947808a266498f36
   var url = 'https://newsapi.org/v2/top-headlines?sources=google-news-it,la-repubblica,ansa,il-sole-24-ore,bbc-news,the-new-york-times,techcrunch,polygon,the-next-web,the-verge,google-news&apiKey=3091d29cdba743aa947808a266498f36&sortBysAvailable=latest&pageSize='+pageSize;
-  redditing = false;
   loadArticlesFromUrl(url);
-}
-
-function loadItalianNews(pageSize = 30) {
-  //NEWS API: 3091d29cdba743aa947808a266498f36
-  var url = 'https://newsapi.org/v2/top-headlines?country=it&apiKey=3091d29cdba743aa947808a266498f36&sortBysAvailable=latest&pageSize='+pageSize;
-  redditing = false;
-  loadArticlesFromUrl(url);
-}
-
-function openAllFavs() {
-  for(var i=0; i<FAVS.list.length; i++)
-  {
-    window.open(FAVS.list[i].url, '_blank');
-  }
 }
 
 function loadConfig() {
   $.getJSON('../config/cmd.json', function(data) {
     CMD_CONFIG = data;
-  });
-  $.getJSON('../config/favorites.json', function(data) {
-    FAVS = data;
   });
 }
 
@@ -139,43 +65,23 @@ $(document).ready(function() {
   loadConfig();
   loadNews(30);
   $("#searchbar").focus();
+  
   $("#search").on('submit', function (e) {
     e.preventDefault();
     var query = $("#searchbar").val();
     var cmd = 0;
-    var cmdDetected = query.charAt(0)=="!";
 
-
-    //QUERY CMDs
-    if(cmdDetected)
-    {
-      query = query.substring(1);
-      query = query.split(" ");
-      cmd = getCmdAction(CMD_CONFIG, query[0]);
-      if(cmd == 0) cmd =-1; //CMD NOT FOUND
-      query.shift();
-      query = query.join(" ");
-    }
+    query = query.split(" ");
+    cmd = getCmdAction(CMD_CONFIG, query[0]);
+    if(cmd == 0) cmd =-1; //CMD NOT FOUND
+    query.shift();
+    query = query.join(" ");
     
     switch(cmd)
     {
-      case -1:
-        break;
-      case 0:
-        var win = window.open("http://www.google.com/search?q="+encodeURIComponent(query), '_blank');
- 	  	win.focus();
-        break;
-
-      //SPECIAL CMDs
       case 1:
         $("#searchbar").val('');
-        redditing = false;
-        loadNews(25);
-        break;
-
-      case 2:
-        $("#searchbar").val('');
-        openAllFavs();
+        loadNews(30);
         break;
 
       case 3:
@@ -187,17 +93,8 @@ $(document).ready(function() {
         $("#searchbar").val('');
         more();
         break;
-
-	  case "https://www.reddit.com/r/":
-        loadSubReddit(query);
-        break;
        
-      case "italian":
-      	loadItalianNews(30);
-        break;
-
       default:
-      	redditing = false;
         var win = window.open(cmd.replace("*", encodeURIComponent(query)), '_blank');
  	  	win.focus();
     }
@@ -210,8 +107,4 @@ $(document).on('keypress', function (e) {
     $("#searchbar").val('');
     $("#searchbar").focus();
   }
-          if(currentArticle>=0 && e.keyCode == 110) //right
-        	readArticle(currentArticle+1);
-        else if(currentArticle>=0 && e.keyCode == 112) //left
-        	readArticle(currentArticle-1);
 });
